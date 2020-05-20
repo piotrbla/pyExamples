19c60fa03e76dd26e5287b7bac43d6adcf238e5b
Piotr B <piotrb@gmail.com>
2020-05-20 23:43:29 +0200

    Quasi animation

--- tkinterMaze.py
+++ tkinterMaze.py
@@ -1,6 +1,9 @@
 from tkinter import *
 
 MARGIN = 20
+ROOM_SIZE = 60
+WALL_SIZE = ROOM_SIZE // 2 - 6
+level_map = []
 
 
 class Room:
@@ -40,23 +43,49 @@ def create_walls(drawing, x, y):
     drawing.create_line(x_end, y_begin_wall, x_end, y_end, fill='blue')
 
 
-def write_number(rysunek, x_val, y_val, i):
+def write_number(drawing, x_val, y_val, i, fill):
     bigger_font = ('arial', 12, 'bold')
-    canvas.create_text(x_val + WALL_SIZE+5, y_val + WALL_SIZE+7, text=str(i), font=bigger_font)
 
+    x_with_wall_size = x_val + WALL_SIZE
+    y_with_wall_size = y_val + WALL_SIZE
+    room_size_without_wall = ROOM_SIZE - 2
+    drawing.create_rectangle(x_val + 2, y_val + 2, x_val + room_size_without_wall
+                             , y_val + room_size_without_wall, fill=fill, outline=fill)
+    drawing.create_text(x_with_wall_size + 5, y_with_wall_size + 7, text=str(i), font=bigger_font)
+
+
+def brute_force():
+    from time import sleep
+    fill_room = 'light grey'
+    fill_wall = 'saddle brown'
+    for j in range(10):
+        i = 0
+        x_val = MARGIN
+        y_val = MARGIN
+        for row in level_map:
+            for room in row:
+                if room.is_wall:
+                    fill = fill_wall
+                else:
+                    fill = fill_room
+                write_number(canvas, x_val, y_val, i+j, fill)
+                x_val += ROOM_SIZE + 2
+                i += 1
+            x_val = MARGIN
+            y_val += ROOM_SIZE + 1
+        canvas.update_idletasks()
+        sleep(0.1)
 
 tk_window = Tk()
 Label(tk_window, text='Labirynty').grid(row=0)
 Label(tk_window, text='Rekurencja').grid(row=1)
-Button(tk_window, text='Rozwiąż brute-force').grid(row=0, column=1, sticky=W)
+Button(tk_window, text='Rozwiąż brute-force', command=brute_force).grid(row=0, column=1, sticky=W)
 Button(tk_window, text='Rozwiąż inaczej').grid(row=1, column=1, sticky=W)
 frame = Frame(tk_window)
 canvas = Canvas(frame, bg='cornsilk', width=1648, height=1268)
 canvas.pack(fill=X)
 arial_font = ('arial', 8, 'bold')
 # canvas.create_text(190, 8, text='Piszę sobie po rysunku, Zażółć gęślą jaźń.', font=arial_font)
-x_val = MARGIN
-y_val = MARGIN
 rows = ["..#.................",
         "....#....#####......",
         "......##.........#..",
@@ -76,57 +105,60 @@ rows = ["..#.................",
         "................#..."]
 
 rows_big = ["..#.................",
-        "....#....#####......",
-        "......##.........#..",
-        ".......#........#...",
-        "...#...#............",
-        ".......#.......#....",
-        ".......#......#.....",
-        "....#...............",
-        ".............#......",
-        "............#.......",
-        ".....#..............",
-        ".......#............",
-        "...........#........",
-        "......#.............",
-        "......#...#.........",
-        "..............#.....",
-        ".....#....#.........",
-        "...............#....",
-        ".........#..........",
-        "................#..."]
+            "....#....#####......",
+            "......##.........#..",
+            ".......#........#...",
+            "...#...#............",
+            ".......#.......#....",
+            ".......#......#.....",
+            "....#...............",
+            ".............#......",
+            "............#.......",
+            ".....#..............",
+            ".......#............",
+            "...........#........",
+            "......#.............",
+            "......#...#.........",
+            "..............#.....",
+            ".....#....#.........",
+            "...............#....",
+            ".........#..........",
+            "................#..."]
 
-ROOM_SIZE = 60
-WALL_SIZE = ROOM_SIZE // 2 - 6
-fill_room = 'light grey'
-fill_wall = 'saddle brown'
-level_map = []
-for row in rows:
-    level_row = []
-    for c in row:
-        room = Room()
-        if c == '#':
-            room.is_wall = True
-        level_row.append(room)
-    level_map.append(level_row)
 
-i = 0
+def main():
+    fill_room = 'light grey'
+    fill_wall = 'saddle brown'
+    for row in rows:
+        level_row = []
+        for c in row:
+            room = Room()
+            if c == '#':
+                room.is_wall = True
+            level_row.append(room)
+        level_map.append(level_row)
 
-for row in level_map:
-    for room in row:
-        if room.is_wall:
-            fill = fill_wall
-        else:
-            fill = fill_room
-        canvas.create_rectangle(x_val, y_val, x_val + ROOM_SIZE, y_val + ROOM_SIZE, fill=fill, outline=fill)
-        create_walls(canvas, x_val, y_val)
-        # write_number(canvas, x_val, y_val, i)
-        # canvas.create_text(xVal, yVal, text=line + str(row), font=('arial', 8, 'bold'))
-        x_val += ROOM_SIZE + 2
-        i += 1
+    i = 0
     x_val = MARGIN
-    y_val += ROOM_SIZE + 1
+    y_val = MARGIN
 
-frame.grid(row=3, columnspan=2)
-canvas.configure()
-tk_window.mainloop()
+    for row in level_map:
+        for room in row:
+            if room.is_wall:
+                fill = fill_wall
+            else:
+                fill = fill_room
+            canvas.create_rectangle(x_val, y_val, x_val + ROOM_SIZE, y_val + ROOM_SIZE, fill=fill, outline=fill)
+            create_walls(canvas, x_val, y_val)
+            x_val += ROOM_SIZE + 2
+            i += 1
+        x_val = MARGIN
+        y_val += ROOM_SIZE + 1
+
+    frame.grid(row=3, columnspan=2)
+    canvas.configure()
+    tk_window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
